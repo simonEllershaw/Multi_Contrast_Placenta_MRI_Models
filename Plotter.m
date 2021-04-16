7a67609966a464d2cbcd7033317aa85181ecdb4b
Simon Ellershaw <simje@live.co.uk>
2021-04-16 11:55:32 +0100

    BIC and continum model proof of concept

--- Plotter.m
+++ Plotter.m
@@ -1,10 +1,59 @@
 classdef Plotter
     methods(Static)
+        function compare_models(param_maps, V_h_slice, V_pe_slice, model_labels)
+            % Extract size of map and setup plot
+            num_maps = size(param_maps, 1);
+            num_models = size(param_maps, 2);
+            
+            % Convert 2D slices into grayscale RGB array
+            V_h_slice_rgb = double2rgb(flipud(V_h_slice), gray, []);
+            % Zero pad so h and pe slices are the same size
+            V_pe_slice_rgb = zeros(size(V_h_slice_rgb));
+            V_pe_slice_rgb(1:size(V_pe_slice,1), 1:size(V_pe_slice,2), :) = double2rgb(flipud(V_pe_slice), gray, []);
+            
+            
+            for map_num=1:num_maps
+                % Alternate which slice is the background
+                if mod(map_num,2)==1
+                    slice_rgb = V_h_slice_rgb;
+                else
+                    slice_rgb = V_pe_slice_rgb;
+                end
+                % Plot combined rgb map
+                map_model_1 = flipud(squeeze(param_maps(map_num,1,:,:))');
+                map_model_2 = flipud(squeeze(param_maps(map_num,2,:,:))');
+                map_combine = slice_rgb;
+                for i=1:size(map_model_1,1)
+                    for j=1:size(map_model_1,2)
+                        if map_model_2(i,j) < map_model_1(i,j)
+                            map_combine(i,j,:) = [1,0,0];
+                        elseif map_model_1(i,j) < map_model_2(i,j)
+                            map_combine(i,j,:) = [0,0,1];
+                        end
+                    end
+                end
+                
+                nexttile;
+                imshow(map_combine)
+%                 if map_num == num_maps
+%                     colormap(hot);
+%                     caxis([param_min, param_max]);
+%                     cb = colorbar;
+%                     cb.Label.String = param_units(param_num);
+%                 end
+%                 if param_num == 1
+%                     title(map_labels(map_num));
+%                 end
+%                 if map_num == 1
+%                     ylabel(param_labels(param_num), 'Rotation',0);
+%                 end
+            end
+        end
+            
         function visualise_param_maps(param_maps, V_h_slice, V_pe_slice, param_labels, map_labels, param_units)
             % Extract size of map and setup plot
             num_maps = size(param_maps, 1);
             num_params = size(param_maps, 2);
-            tiledlayout(num_params,num_maps);
             
             % Convert 2D slices into grayscale RGB array
             V_h_slice_rgb = double2rgb(flipud(V_h_slice), gray, []);
@@ -61,7 +110,7 @@ classdef Plotter
         function [param_min, param_max] = thresholdParameters(param_map)
             param_map_sorted = sort(param_map(:));
             % Set upper bound as nth highest value to handle outliers
-            param_map_no_outliers = param_map_sorted(1:end-50);
+            param_map_no_outliers = param_map_sorted(1:end-100);
             param_min = min(param_map_no_outliers(param_map_no_outliers~=0));
             param_max = param_map_no_outliers(end);
         end
